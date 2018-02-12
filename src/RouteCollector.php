ec8c5407e4331dc3b769d31be58e271e978720de
kento-oka <kento.oka@kentoka.com>
2018-02-12 20:32:46 +0900

    新しい構造に変更

--- src/RouteCollector.php
+++ src/RouteCollector.php
@@ -18,180 +18,85 @@ namespace Fratily\Router;
  */
 class RouteCollector{
 
-    const RAW   = 1;
-    const REG   = 2;
-    const SREG  = 3;
-    
-    const REG_SEG   = "/\A\{([A-Z_][0-9A-Z_]*)(:|\|)(.+?)\}\z/i";
-    
     /**
-     * @var mixed[][]
+     * @var mixed[]
      */
-    private $rule   = [];
-    
+    private $routes = [];
+
     /**
-     * @var mixed[][]
+     * @var string[]
      */
-    private $node   = [];
-    
-    
-    public function addRoute(string $method, string $path, array $data = []){
-        $segments   = Parser::split2segments(explode("?", $path, 2)[0]);
-        $i          = 1;
-        $parent     = "root";
-        $static     = true;
-        
-        foreach($segments as $segment){
-            $name   = null;
-            
-            if((bool)preg_match(self::REG_SEG, $segment, $m)){
-                $static = false;
-                
-                if($m[2] === ":"){
-                    $ruleId = $this->addRule(self::REG, $m[3]);
-                    $name   = $m[1];
-                }else{
-                    $ruleId = $this->addRule(self::SREG, $m[3]);
-                    $name   = $m[1];
-                }
-            }else{
-                $ruleId = $this->addRule(self::RAW, $segment);
-            }
-            
-            $parent = $this->addNode($i++, $ruleId, $parent, $method, $name);
-        }
-        
-        if($static){
-            $this->static[$method]      = $this->static[$method] ?? [];
-            $this->static[$method]["/".implode("/", $segments)] = $data;
-        }
-        
-        if($this->node[$parent]["data"] !== null){
-            $this->node[$parent]["data"]    = array_merge(
-                $this->node[$parent]["data"], $data
-            );
-        }else{
-            $this->node[$parent]["data"]    = $data;
-        }
-    }
-    
+    private $paths  = [];
+
     /**
-     * ルールを取得する
-     * 
-     * @param   string  $id
-     * 
-     * @return  mixed[]|null
+     * ルートを追加する
+     *
+     * 同じパスを使用するルートが定義された場合、
+     * 前に定義されたルートは上書きされる。
+     *
+     * @param   string      $name
+     * @param   string      $path
+     * @param   string[]    $methods    [optional]
+     *      許容するHTTPメソッドを持つ配列。
+     *      nullを指定した場合はすべてのメソッドを許容する。
+     * @param   mixed[] $data   [optional]
+     *      ルートに一致した場合に返される値。
+     *
+     * @return  void
+     *
+     * @throws  \LogicException
      */
-    public function getRule(string $id){
-        return $this->rule[$id] ?? null;
-    }
-    
-    /**
-     * ルールの存在を確認する
-     * 
-     * @param   string  $id
-     */
-    public function hasRule(string $id){
-        return isset($this->rule[$id]);
-    }
-    
-    /**
-     * ルールを追加する
-     * 
-     * @param   int $type
-     * @param   string  $match
-     * 
-     * @return  string
-     *      ルールIDが返される。
-     * 
-     * @throws  \InvalidArgumentException
-     */
-    protected function addRule(int $type, string $match){
-        if($type !== self::RAW && $type !== self::REG && $type !== self::SREG){
-            throw new \InvalidArgumentException();
-        }
-        
-        //  ここの正規表現はパフォーマンスやその他もろもろとの兼ね合いで調整
-        if($type === self::REG && (bool)preg_match("/\A[0-9A-Z-_]\z/i", $match)){
-            $type   = self::RAW;
-        }
-        
-        $id = hash("md5", $type . $match);
-        
-        if(!isset($this->rule[$id])){
-            $this->rule[$id]    = [
-                "type"  => $type,
-                "match" => $match
-            ];
-        }
-        
-        return $this->rule[$id];
-    }
-    
-    /**
-     * ノードを取得する
-     * 
-     * @param   string  $id
-     * 
-     * @return  mixed[]
-     */
-    public function getNode(string $id){
-        return $this->node[$id] ?? null;
-    }
-    
-    /**
-     * ノードの存在を確認する
-     * 
-     * @param   string  $id
-     * 
-     * @return  bool
-     */
-    public function hasNode(string $id){
-        return isset($this->node);
-    }
-    
-    /**
-     * ノードを追加する
-     * 
-     * @param   int $row
-     *      木構造での階層
-     * @param   string  $ruleId
-     * @param   string  $parentId
-     * @param   string  $method
-     * @param   string  $name
-     * 
-     * @return  string
-     * 
-     * @throws  \InvalidArgumentException
-     */
-    protected function addNode(
-        int $row,
-        string $ruleId,
-        string $parentId,
-        string $method,
-        string $name = null
+    public function addRoute(
+        string $name,
+        string $path,
+        array $methods = null,
+        array $data = []
     ){
-        if(!$this->hasRule($ruleId)){
-            throw new \InvalidArgumentException();
-        }else if(!isset($this->node[$parentId])){
-            throw new \InvalidArgumentException();
+        if(isset($this->routes[$name])){
+            throw new \LogicException;
         }
-        
-        $nodeId = hash("md5", $parentId . $row . $ruleId . $method . ($name ?? ""));
-        
-        if(!isset($this->node[$nodeId])){
-            $this->node[$nodeId]    = [
-                "rule"  => $ruleId,
-                "name"  => $name,
-                "data"  => null,
-                "child" => []
-            ];
-            
-            if(!in_array($nodeId, $this->node[$parentId]["child"])){
-                $this->node[$parentId]["child"][]   = $nodeId;
-            }
+
+        $path       = substr($path, 0, 1) !== "/" ? "/{$path}" : $path;
+        $methods    = $methods === null ? null : array_unique(
+            array_map("strtoupper",
+                array_filter(
+                    $methods,
+                    function($v){
+                        return is_string($v) && $v !== "";
+                    }
+                )
+            )
+        );
+
+        if(isset($this->paths[$path])){
+            unset($this->routes[$this->paths[$path]]);
         }
-        
-        return $nodeId;
+
+        $this->paths[$path]     = $name;
+        $this->routes[$name]    = [
+            "path"  => $path,
+            "allow" => ($methods === null || empty($methods)) ? null : $methods,
+            "data"  => $data
+        ];
+    }
+
+    public function get(string $name, string $path, array $data = []){
+        $this->addRoute($name, $path, ["GET"], $data);
+    }
+
+    public function post(string $name, string $path, array $data = []){
+        $this->addRoute($name, $path, ["POST"], $data);
+    }
+
+    public function put(string $name, string $path, array $data = []){
+        $this->addRoute($name, $path, ["PUT"], $data);
+    }
+
+    public function patch(string $name, string $path, array $data = []){
+        $this->addRoute($name, $path, ["PATCH"], $data);
+    }
+
+    public function delete(string $name, string $path, array $data = []){
+        $this->addRoute($name, $path, ["DELETE"], $data);
     }
 }
