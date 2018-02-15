13d174bbcaf9139c9e88482792b6de97ce371694
kento-oka <kento.oka@kentoka.com>
2018-02-15 18:10:54 +0900

    セグメントを解析するメソッド追加

--- src/Parser.php
+++ src/Parser.php
@@ -30,4 +30,33 @@ class Parser{
     public static function split2segments(string $url){
         return explode("/", substr($url, 0, 1) === "/" ? substr($url, 1) : $url);
     }
+
+    /**
+     * セグメント文字列を解析する
+     *
+     * @param   string  $segment
+     *
+     * @return  mixed[]
+     */
+    public static function segment(string $segment){
+        $type   = Router::RAW;
+        $param  = null;
+        $match  = $segment;
+
+        if((bool)preg_match("/\A([0-9A-Z_]+)(:|\|)(.+?)\z/i", $segment, $m)){
+            $type   = $m[2] === ":" ? Router::REG : Router::SREG;
+            $param  = $m[1];
+            $match  = $m[3];
+
+            if($type === Router::REG && (bool)preg_match("/\A[0-9A-Z%_]*\z/i", $match)){
+                $type   = Router::RAW;
+            }
+        }
+
+        return [
+            "type"  => $type,
+            "param" => $param,
+            "match" => $match
+        ];
+    }
 }
